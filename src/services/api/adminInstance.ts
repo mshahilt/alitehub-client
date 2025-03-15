ec78682f3d03fb3cf8e3bed79dfc8c1ddc7367b6
mshahilt <mshahilt3@gmail.com>
2025-03-15 11:55:44 +0530

    video call test_1

--- src/services/api/adminInstance.ts
+++ src/services/api/adminInstance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 const adminAxiosInstance: AxiosInstance = axios.create({
-    baseURL: "https://fd92-103-170-228-58.ngrok-free.app/admin",
+    baseURL: "https://de13-103-170-228-58.ngrok-free.app/admin",
     headers: {
         "Content-Type": "application/json",
     },
--- src/services/api/userInstance.ts
+++ src/services/api/userInstance.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 import { setupInterceptors } from "./interceptor";
 
 const axiosInstance: AxiosInstance = axios.create({
-    baseURL: "https://fd92-103-170-228-58.ngrok-free.app/",
+    baseURL: "https://de13-103-170-228-58.ngrok-free.app/",
     headers: {
         "Content-Type": "application/json",
     },
